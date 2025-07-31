169f43910a7a4066380a6c58479bed7c7110c469
Macheng <macheng97@126.com>
2025-07-31 15:53:27 +0800

    add test and read toml

---
+++ src/radialreal/config/__init__.py
@@ -0,0 +1,26 @@
+import logging
+import tomllib
+import pprint
+from collections import OrderedDict
+from pathlib import Path
+
+__all__ = ['load_config', 'print_config']
+
+def load_config(path: Path) -> dict:
+    """ load input toml"""
+    try:
+        with open(path, "rb") as f:
+            return tomllib.load(f)
+    except FileNotFoundError:
+        logging.error(f"File {path} not found")
+        raise
+    except tomllib.TOMLDecodeError as e:
+        logging.error(f"File {path} error : {e}")
+        raise
+
+def print_config(config: dict) -> None:
+    custom_order = ['system', 'theory', 'potential', 'solver', 'basis']
+    ordered_config = OrderedDict()
+    for key in custom_order:
+        ordered_config[key] = config[key]
+    pprint.pprint(ordered_config)
--- src/radialreal/main.py
+++ src/radialreal/main.py
@@ -1,4 +1,7 @@
 import argparse
+from pathlib import Path
+
+from radialreal.config import load_config,print_config
 
 
 def main():
@@ -8,8 +11,11 @@ def main():
     )
     parser.add_argument('-i', '--input')
     args = parser.parse_args()
-    print(args.input)
-    print("begin")
+    print("### RR begin o0v0o ###")
+    # read input file
+    config=load_config(Path(args.input))
+    print_config(config)
+    # print input file
 
 if __name__ == '__main__':
     main()
